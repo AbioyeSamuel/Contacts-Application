1e5053044301c00fc7dc47d4174c7142a4f571f7
AbioyeSamuel <101730798+AbioyeSamuel@users.noreply.github.com>
2022-05-02 02:19:23 -0700

    Add files via upload
    A contact application software that takes input of name, mobile number and stores other data

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module Contacts {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.xml;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires java.compiler;
+
+
+
+    opens sample;
+}
